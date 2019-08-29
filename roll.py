4c7029e9509d7cac4e9077489f04c3e2a807a65c
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-08-29 16:44:09 +0530

    Update roll.py

--- roll.py
+++ roll.py
@@ -2,7 +2,7 @@ from pyrogram import Client, Filters, Emoji
 import random
 import time
 
-app = ("session",bot_token="820881097:AAHM0xwwTv1HVztMZ9PHLktj-2AAwBXZdVI",api_id=605563,api_hash="7f2c2d12880400b88764b9b304e14e0b") 
+app = Client("session",bot_token="820881097:AAHM0xwwTv1HVztMZ9PHLktj-2AAwBXZdVI",api_id=605563,api_hash="7f2c2d12880400b88764b9b304e14e0b") 
 
 
 @app.on_message(Filters.command('ani'))
