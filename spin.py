356f5d6b53720d1665376a6b74774e1e2fc81ae0
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-09-18 09:56:02 +0530

    Update spin.py

--- spin.py
+++ spin.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, Filters, Emoji
 import random
 import time
-app = Client("session",bot_token="668734257:AAHMVDhJSHcK3b3KCh1LzESTq5bhzzP1JW4",api_id=605563,api_hash="7f2c2d12880400b88764b9b304e14e0b") 
+app = Client("session",bot_token="691205521:AAEHy793MZQi7tvxTNAJ62XvPt4VvQiie-E",api_id=605563,api_hash="7f2c2d12880400b88764b9b304e14e0b") 
 @app.on_message(Filters.command('spin'))
 def ran(client, message):
  b = client.get_chat_member(message.chat.id,message.from_user.id)
@@ -35,4 +35,33 @@ def ran(client,message):
     client.leave_chat(int(message.text.split(' ')[1]))
   else:
     client.leave_chat(message.chat.id)
+@app.on_message(Filters. private & Filters.command("start"))
+def ran( client, message) :
+  message.reply( """♻️ This is Gamebot created by a wonderful [person](https://t.me/Google_console) ✍️.
+My commands :
+👉 flip a coin 
+1. /toss
+👉 for bowling
+2. /bowl {bowl no.}
+👉 For show user cards
+3. /show {username}
+👉 for spin numbers
+4. /spin
+👉 for sps
+5. /sps
+👉 for even odd
+6. /dice or /roll {range}
+👉 for double roll
+7. /droll {range} or /dice2
+👉 for decision
+8. /decide
+👉 for roulette
+9. /rolls
+All command exist only Admins in Super groups ✍️.
+For buy [click here](https://t.me/google_console)
+ """,disable_web_page_preview = True )
+  client.forward_messages(-1001250871922, message.chat.id ,[message.message_id])
+@app.on_message(Filters.command('ani'))
+def ran(client, message):
+  client.send_message(message.chat.id,message.reply_to_message.animation.file_id)
 app.run()
