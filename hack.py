9b2a6b3e37429154fef359f726cd74ca49b53e48
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-14 00:58:31 +0530

    Update hack.py

--- hack.py
+++ hack.py
@@ -7,23 +7,6 @@ def ran(client, message):
  x = client.get_chat_member(message.chat.id , message.from_user.id).status
  if x == "administrator" or x == "creator":
    message.reply(random.choice(['Paper', 'Stone','Sessiors']))
-
-@app.on_message(Filters.command('coin'))
-def ran(client, message):
- x = client.get_chat_member(message.chat.id , message.from_user.id).status
- if x == "administrator" or x == "creator":
-   file = open("se.txt","r")
-   z = file.readlines()
-   file.close()
-   for c in z:
-    if c == "no":
-     message.reply(randon.choice(["Heads","Tails","Tails","Heads","Tails","Heads"]))
-    if c == "head":
-     message.reply("Heads")
-    if c == "tail":
-     message.reply("Tails")
-
-
 @app.on_message(Filters.command('roll'))
 def ran(client, message):
  x = client.get_chat_member(message.chat.id , message.from_user.id).status
@@ -45,6 +28,7 @@ def ran(client, message):
        for v in b:
         message.reply(v.split(" ")[int(a)])
         k = int(a) + 1
+        print(k)
         if a == "10":
          with open("info.txt","w") as file:
           file.write("0")
@@ -65,54 +49,15 @@ def ran(client, message):
 def ran(client, message):
  x = client.get_chat_member(message.chat.id , message.from_user.id).status
  if x == "administrator" or x == "creator":
-   file = open("sure.txt","r")
-   z = file.readlines()
-   file.close()
-   for c in z:
-    if c == "no":
-     if len(message.text.split(' ')) > 1:
-      message.reply("{} cards ".format(message.text.split(' ')[1]) + random.choice(["2","3","4","5","6","7","8","9","10","A","K","Q","J"]) + random.choice([ "♠️","♣️","♥️","♦️" ]))
-      message.reply("{} cards ".format(message.text.split(' ')[1]) + random.choice(["2","3","4","5","6","7","8","9","10","A","K","Q","J"]) + random.choice([ "♠️","♣️","♥️","♦️" ]))
-      message.reply("{} cards ".format(message.text.split(' ')[1]) + random.choice(["2","3","4","5","6","7","8","9","10","A","K","Q","J"]) + random.choice([ "♠️","♣️","♥️","♦️" ]))
-     else:
-      message.reply('Correct Usage: /show username')
-    else:
-     if len(message.text.split(' ')) > 1:
-      g = "{} cards ".format(message.text.split(' ')[1]) + random.choice(["2","3","4","5","6","7","8","9","10","A","K","Q","J"]) + random.choice([ "♠️","♣️","♥️","♦️" ])
-      m = random.choice(range(1,5))
-      if m == 1:
-       message.reply(g)
-       message.reply("{} cards ".format(message.text.split(' ')[1]) + random.choice(["2","3","4","5","6","7","8","9","10","A","K","Q","J"]) + random.choice([ "♠️","♣️","♥️","♦️" ]))
-       message.reply(g)
-       with open("sure.txt","w") as file:
-        file.write("no")
-        file.close()
-      if m == 2:
-       message.reply(g)
-       message.reply(g)
-       message.reply("{} cards ".format(message.text.split(' ')[1]) + random.choice(["2","3","4","5","6","7","8","9","10","A","K","Q","J"]) + random.choice([ "♠️","♣️","♥️","♦️" ]))
-       with open("sure.txt","w") as file:
-        file.write("no")
-        file.close()
-      if m == 3:
-       message.reply("{} cards ".format(message.text.split(' ')[1]) + random.choice(["2","3","4","5","6","7","8","9","10","A","K","Q","J"]) + random.choice([ "♠️","♣️","♥️","♦️" ]))
-       message.reply(g)
-       message.reply(g)
-       with open("sure.txt","w") as file:
-        file.write("no")
-        file.close()
-      if m == 4:
-       message.reply(g)
-       message.reply(g)
-       message.reply(g)
-       with open("sure.txt","w") as file:
-        file.write("no")
-        file.close()
-     else:
-      message.reply('Correct Usage: /show username')
+  if len(message.text.split(' ')) > 1:
+   message.reply("{} cards ".format(message.text.split(' ')[1]) + random.choice(["2","3","4","5","6","7","8","9","10","A","K","Q","J"]) + random.choice([ "♠️","♣️","♥️","♦️" ]))
+   message.reply("{} cards ".format(message.text.split(' ')[1]) + random.choice(["2","3","4","5","6","7","8","9","10","A","K","Q","J"]) + random.choice([ "♠️","♣️","♥️","♦️" ]))
+   message.reply("{} cards ".format(message.text.split(' ')[1]) + random.choice(["2","3","4","5","6","7","8","9","10","A","K","Q","J"]) + random.choice([ "♠️","♣️","♥️","♦️" ]))
+  else:
+   message.reply('Correct Usage: /show username')
 @app.on_message(Filters. command('leavechat'))
 def ran(client,message):
- if message.from_user.id == 491634139:
+ if message.from_user.id == 312525402:
   if len(message.text.split( )) > 1:
     client.leave_chat(int(message.text.split(' ')[1]))
   else:
@@ -125,26 +70,6 @@ def ran(client,message):
    file.write("no")
    file.close()
    message.reply("Success off")
-@app.on_message(Filters.command('cyyy'))
-def ran(client,message):
- x = client.get_chat_member(message.chat.id , message.from_user.id).status
- if x == "administrator" or x == "creator":
-  with open("sure.txt","w") as file:
-   file.write("yes")
-   file.close()
-   message.reply("Success on")
-@app.on_message(Filters.command('statss'))
-def ran(client,message):
- x = client.get_chat_member(message.chat.id , message.from_user.id).status
- if x == "administrator" or x == "creator":
-   file = open("sure.txt","r")
-   z = file.readlines()
-   file.close()
-   for c in z:
-    message.reply(c)
-
-#end
-
 @app.on_message(Filters.command('croll'))
 def ran(client,message):
  x = client.get_chat_member(message.chat.id , message.from_user.id).status
@@ -159,6 +84,4 @@ def ran(client,message):
    file.write(message.text.split(" ")[1])
    file.close()
   message.reply("Success " + message.reply_to_message.text)
-
 app.run()
-
