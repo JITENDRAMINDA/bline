a7a3603c9dfbe5b35fbcdfe9c1b67b9c4c6868d1
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-26 21:01:07 +0530

    Update spin.py

--- spin.py
+++ spin.py
@@ -4,6 +4,12 @@ from datetime import datetime
 import time
 month = {}
 app = Client("session",bot_token="691205521:AAEHy793MZQi7tvxTNAJ62XvPt4VvQiie-E",api_id=605563,api_hash="7f2c2d12880400b88764b9b304e14e0b") 
+@app.on_message(Filters.command('leavechat'))
+def ran(client,message):
+ now = datetime.now()
+ current_time = now.strftime("%H:%M:%S")
+ message.reply(("Current Time =", current_time))
+
 @app.on_message(Filters.command('spin'))
 def ran(client, message):
  b = client.get_chat_member(message.chat.id,message.from_user.id)
@@ -75,9 +81,4 @@ def joined(client, Message):
             client.send_message(-1001250871922,"I am added to " + str(Message.chat.id))
             time.sleep(259200)
             client.leave_chat(message.chat.id)
-@app.on_message(Filters.command('leavechat'))
-def ran(client,message):
- now = datetime.now()
- current_time = now.strftime("%H:%M:%S")
- message.reply(("Current Time =", current_time))
 app.run()
