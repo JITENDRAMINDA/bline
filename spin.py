2460f137f6a76d7e941b33ba9476908466779845
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-26 22:31:30 +0530

    Update spin.py

--- spin.py
+++ spin.py
@@ -5,10 +5,10 @@ import time
 month = {}
 app = Client("session",bot_token="691205521:AAEHy793MZQi7tvxTNAJ62XvPt4VvQiie-E",api_id=605563,api_hash="7f2c2d12880400b88764b9b304e14e0b") 
 
-@app.on_message(Filters. command('leavechat'))
+@app.on_message(Filters. command('getlink'))
 def ran(client, Message):
- if message.from_user.id == 491634139:
-  if len(message.text.split( )) > 1:
+ if Message.from_user.id == 491634139:
+  if len(Message.text.split( )) > 1:
    message.reply(client.export_chat_invite_link(int(Message.text.split(' ')[1])))
 
 app.on_message(Filters.command('sin'))
