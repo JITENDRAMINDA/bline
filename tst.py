5d9757d4eadcefb79f7a46578d1816cabd115a98
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-29 08:53:37 -0700

    Update tst.py

--- tst.py
+++ tst.py
@@ -1,9 +1,20 @@
 from pyrogram import Client, Filters, Emoji
 import random
 import time
-app = Client("session",bot_token="663574960:AAGWg1VGruCPuckHzjbpDLRIbPWkX6YcDlc",api_id=605563,api_hash="7f2c2d12880400b88764b9b304e14e0b")  
+app = Client("session",bot_token="622131522:AAHordhdDvQ08qLmEsVl3U4Nkb2S-aEAJr8",api_id=715451,api_hash="d2cba6f7bf5d1a45682da5bb9071a307")
+
+@app.on_message(Filters. private & Filters.command("start"))
+def ran( client, message) :
+  message.reply( """♻️ This is antiusername created by a wonderful [person](https://t.me/Google_console) ✍️.
+Add me to your group i will delete username from non admins automatically.""")
+
+
 @app.on_message(Filters.group)
-def ran(client, message):
+def main(client, message):
  if not message.from_user:
   message.delete(message.message_id)
+ b = client.get_chat_member(message.chat.id , message.from_user.id).status
+ if not b == 'administrator' or not b == "creator": 
+  if "@" in message.text: 
+   client.delete_messages(message.chat.id, message.message_id)
 app.run()
