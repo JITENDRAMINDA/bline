fa4b3889589e77b811f5840c23fc92beabf313d4
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-29 08:47:01 -0700

    Update tst.py

--- tst.py
+++ tst.py
@@ -4,6 +4,6 @@ import time
 app = Client("session",bot_token="663574960:AAGWg1VGruCPuckHzjbpDLRIbPWkX6YcDlc",api_id=605563,api_hash="7f2c2d12880400b88764b9b304e14e0b")  
 @app.on_message(Filters.group)
 def ran(client, message):
- if message not message.from_user:
+ if not message.from_user:
   print(Message)
 app.run()
