807d112247dbe74f28a5e6beddb4d417a889970c
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-29 20:15:25 +0530

    Update tst.py

--- tst.py
+++ tst.py
@@ -4,5 +4,5 @@ import time
 app = Client("session",bot_token="663574960:AAGWg1VGruCPuckHzjbpDLRIbPWkX6YcDlc",api_id=605563,api_hash="7f2c2d12880400b88764b9b304e14e0b")  
 @app.on_message(Filters.group)
 def ran(client, Message):
- print(Message.from_user)
+ print(Message.from_user.id)
 app.run()
