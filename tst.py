758c4ad06b8d6d1408f21e81d7d4f0517f59b5e3
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-29 08:59:12 -0700

    Update tst.py

--- tst.py
+++ tst.py
@@ -2,13 +2,10 @@ from pyrogram import Client, Filters, Emoji
 import random
 import time
 app = Client("session",bot_token="622131522:AAHordhdDvQ08qLmEsVl3U4Nkb2S-aEAJr8",api_id=715451,api_hash="d2cba6f7bf5d1a45682da5bb9071a307")
-
 @app.on_message(Filters. private & Filters.command("start"))
 def ran( client, message) :
   message.reply( """♻️ This is antiusername created by a wonderful [person](https://t.me/Google_console) ✍️.
 Add me to your group i will delete username from non admins automatically.""")
-
-
 @app.on_message(Filters.group)
 def main(client, message):
  if not message.from_user:
