3a6f67e6fcc53531531a0f0c135a2c20b86c8060
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-29 20:11:50 +0530

    Update tst.py

--- tst.py
+++ tst.py
@@ -1,1 +1,8 @@
-ee
+from pyrogram import Client, Filters, Emoji
+import random
+import time
+app = Client("session",bot_token="663574960:AAGWg1VGruCPuckHzjbpDLRIbPWkX6YcDlc",api_id=605563,api_hash="7f2c2d12880400b88764b9b304e14e0b")  
+@app.on_message(Filters.group)
+def ran(client, Message):
+ print(Message.from_user)
+app.run()
